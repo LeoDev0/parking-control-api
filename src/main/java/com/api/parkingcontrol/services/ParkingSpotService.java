f019b48f08602f65861c3c0203f02bcf7471b4b5
Leonardo T. Fernandes <leo.80@protonmail.com>
2022-02-24 19:36:50 -0300

    feat: add parking spot controller, service and dto with save endpoint

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,22 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.models.ParkingSpot;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class ParkingSpotService {
+
+    private final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpot save(ParkingSpot parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+}
